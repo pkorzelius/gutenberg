662918207478aeac639ac4b9b5e0ba1e6920bebb
Dennis Snell <dennis.snell@automattic.com>
2022-10-22 13:27:22 -0700

    Poll every ten minutes; once a minute still stalled

--- bin/plugin/commands/performance.js
+++ bin/plugin/commands/performance.js
@@ -198,7 +198,7 @@ async function runPerformanceTests( branches, options ) {
 		const [ s, ns ] = process.hrtime();
 		log( '*****' + JSON.stringify( [ s + ns / 1e9, os.cpus() ] ) );
 
-		setTimeout( reportCpuUsage, 60000 );
+		setTimeout( reportCpuUsage, 10 * 60 * 1000 );
 	};
 
 	reportCpuUsage();
