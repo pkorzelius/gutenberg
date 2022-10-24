1e0a373356c803fd73bb55f7ef4d40ec878d4a0a
Dennis Snell <dennis.snell@automattic.com>
2022-10-24 13:17:56 -0700

    And fetch the remote refs too

--- bin/plugin/commands/performance.js
+++ bin/plugin/commands/performance.js
@@ -218,10 +218,10 @@ async function runPerformanceTests( branches, options ) {
 		.raw( 'init' )
 		.raw( 'remote', 'add', 'origin', config.gitRepositoryURL );
 
-	// for ( const ref of refs ) {
-	// 	await git.raw( 'fetch', '--depth=2', 'origin', ref );
-	// }
 	await git.raw( 'fetch' );
+	for ( const ref of refs ) {
+		await git.raw( 'fetch', 'origin', ref );
+	}
 
 	await git.raw( 'checkout', refs[ 0 ] );
 	await git.raw( 'pull', '-p' );
